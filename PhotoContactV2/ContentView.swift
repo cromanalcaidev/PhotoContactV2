4483c8b2907039967a91dd632ab4d4029f24f9e2
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-06-01 19:40:07 +0200

    Initial Commit

---
+++ PhotoContactV2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoContactV2
+//
+//  Created by Carlos Román Alcaide on 1/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoContactV2/PhotoContactV2App.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoContactV2App.swift
+//  PhotoContactV2
+//
+//  Created by Carlos Román Alcaide on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoContactV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
